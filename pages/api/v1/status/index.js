8b495760f3fc90fd4fe5452df5738432cf761fb8
Gabriela <gabriela05paula@gmail.com>
2025-12-08 14:51:57 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  // versão do PostgreSQL
+  const versionResult = await database.query("SHOW server_version;");
+  const postgresVersion = versionResult.rows[0].server_version;
+
+  //  máximo de conexões
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+
+  // conexões ativas
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const activeConnections = await activeConnectionsResult.rows[0].count;
+
+  const statusInfo = {
+    updated_at: updatedAt,
+    version_bd: postgresVersion,
+    maxConnections_bd: maxConnections,
+    activeConnections_bd: activeConnections,
+  };
+
+  console.log(statusInfo);
+  response.status(200).json(statusInfo);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,26 @@
 test("GET to /api/v1/status returns 200 and correct body", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const body = await response.json();
+
+  // teste updated_at
+  expect(body.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(body.updated_at).toISOString();
+  expect(body.updated_at).toEqual(parsedUpdatedAt);
+
+  // teste versão do PostgreSQL
+  expect(body.version_bd).toBeDefined();
+  expect(typeof body.version_bd).toBe("string");
+  expect(body.version_bd).toMatch(/\d+\.\d+/);
+
+  // número máximo de conexões
+  expect(body.maxConnections_bd).toBeDefined();
+  expect(typeof body.maxConnections_bd).toBe("number");
+  expect(body.maxConnections_bd).toBeGreaterThan(0);
+
+  // número de conexões ativas
+  expect(body.activeConnections_bd).toBeDefined();
+  expect(typeof body.activeConnections_bd).toBe("number");
+  expect(body.activeConnections_bd).toEqual(1);
 });
